2ff9cf9fe655cc840f4f59419edb1e310698eff9
tigerninjaman <tigerninjaman@gmail.com>
2016-04-03 02:04:45 -0700

    Update config_template.js

--- src/config_template.js
+++ src/config_template.js
@@ -2,10 +2,10 @@ var config = {};
 
 config.bots = [
   {
-    botID: 'xxxxxxxxxx',
-    groupID: 'xxxxxxxx',
+    botID: '62e77efd05d8c4ef0c307f201e',
+    groupID: '16884029',
     groupLocalID: '1', // an identifier for this group used in this application only
-    botName: 'mybot',
+    botName: 'ellenbot',
     groupName: 'the-group-name' // optional
   }
 ];
